6690ce484d98dfa5163e3a0046beab185baec587
yukure <yukure@users.noreply.github.com>
2024-04-03 18:34:01 +0900

    fix for raills61

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -4,7 +4,7 @@ require 'action_view/base'
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new({}, {}, "")
+      BestInPlace::ViewHelpers = ActionView::Base.with_empty_template_cache.new({}, {}, "")
     end
   end
 end
